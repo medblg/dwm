b7cb7e194e95dfc66a00409ec98ab358a9c01610
s1m0x <s1m0x@yahoo.com>
2020-11-04 13:18:24 +0100

    sticky

--- config.def.h
+++ config.def.h
@@ -116,6 +116,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	{ MODKEY,                       XK_s,      togglesticky,   {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
