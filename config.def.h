7eaedc7f389ae1a69c8b268b2d124604fb7e2133
s1m0x <s1m0x@yahoo.com>
2020-11-05 17:08:26 +0100

    red color for borders,titles..

--- config.def.h
+++ config.def.h
@@ -33,13 +33,13 @@ enum showtab_modes { showtab_never, showtab_auto, showtab_nmodes, showtab_always
 static const int showtab			= showtab_auto;        /* Default tab bar show mode */
 static const int toptab				= False;               /* False means bottom tab bar */
 static const unsigned int gappx     = 5;        /* gaps between windows */
-static const char *fonts[]          = { "FontAwesome:size=12", "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "FontAwesome:size=12", "monospace:size=11" };
+static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#E45039"; /*005577*/ /*E45039*/
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
